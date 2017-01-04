002e8d8a3d623ca2d3e77537acc7745f4f75da1b
Darrell <624335312@qq.com>
2017-01-04 11:33:44 +0800

    Create auto_next.js

---
+++ auto_next.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName = function(className, element){
+  var children = (element || document).getElementsByTagName('*');
+  var elements = new Array();
+  for (var i=0; i<children.length; i++){
+    var child = children[i];
+    var classNames = child.className.split(' ');
+    for (var j=0; j<classNames.length; j++){
+      if (classNames[j] == className){
+        elements.push(child);
+        break;
+      }
+    }
+  }
+  return elements;
+};
+
+
+setInterval(
+  function() {
+    var els = document.getElementsByClassName('dfss_down');
+    if (els.length == 1){
+      javascript:showNext();
+    };
+  },
+  8000
+);
